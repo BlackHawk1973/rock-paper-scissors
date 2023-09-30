78bacade11a0663b350624ae5a47e77f89e01534
Black Hawk <bhawk73@gmail.com>
2023-09-30 13:55:30 +0300

    Begin

--- src/org/top/Main.java
+++ src/org/top/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-
+        int choiceUser = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Начало игры");
+        System.out.println("1 - Камень");
+        System.out.println("2 - Ножницы");
+        System.out.println("3 - Бумага");
+        System.out.println("0 - Выход");
     }
 }
